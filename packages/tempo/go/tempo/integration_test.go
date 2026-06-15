8275c175fa60cca71a3178a7fbbdcd1a22d2ad1a
Gustavo Ocanto <gustavoocanto@gmail.com>
2026-06-15 16:07:48 +0800

    Move diff tests out of integration_test.go

--- packages/tempo/go/tempo/integration_test.go
+++ packages/tempo/go/tempo/integration_test.go
@@ -67,24 +67,6 @@ func TestDateCases(t *testing.T) {
 	}
 }
 
-func TestDiffInDaysUsesUnixSeconds(t *testing.T) {
-	later, err := Parse("2024-01-03T00:00:00+00:00")
-
-	if err != nil {
-		t.Fatalf("parse later: %v", err)
-	}
-
-	earlier, err := Parse("2024-01-01T00:00:00+00:00")
-
-	if err != nil {
-		t.Fatalf("parse earlier: %v", err)
-	}
-
-	if got := later.DiffInDays(earlier); got != 2 {
-		t.Fatalf("DiffInDays() = %d, want 2", got)
-	}
-}
-
 func TestMutableAndImmutableConversions(t *testing.T) {
 	immutable, err := Parse("2024-02-29T00:00:00+00:00")
 
