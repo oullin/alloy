3dce26f6c0da7f46dca40735720fd5a7d79bdc54
Gustavo Ocanto <gustavoocanto@gmail.com>
2026-06-25 12:26:42 +0800

    Fix Vite formatting CI

--- packages/tempo/tempo-ts/src/types.ts
+++ packages/tempo/tempo-ts/src/types.ts
@@ -37,7 +37,29 @@ export type ComparisonUnit = 'millisecond' | BoundaryUnit;
 
 export type TimeZoneNameStyle = NonNullable<Intl.DateTimeFormatOptions['timeZoneName']>;
 
-export type WeekdayInput = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 'sunday' | 'monday' | 'tuesday' | 'wednesday' | 'thursday' | 'friday' | 'saturday' | 'sun' | 'mon' | 'tue' | 'wed' | 'thu' | 'fri' | 'sat';
+export type WeekdayInput =
+	| 0
+	| 1
+	| 2
+	| 3
+	| 4
+	| 5
+	| 6
+	| 7
+	| 'sunday'
+	| 'monday'
+	| 'tuesday'
+	| 'wednesday'
+	| 'thursday'
+	| 'friday'
+	| 'saturday'
+	| 'sun'
+	| 'mon'
+	| 'tue'
+	| 'wed'
+	| 'thu'
+	| 'fri'
+	| 'sat';
 
 export type TempoOptions = {
 	readonly fallbackLocale?: string;
